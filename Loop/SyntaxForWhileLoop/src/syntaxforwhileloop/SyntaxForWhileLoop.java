5eb865d7833f1079b51d9d587cb7aa0110a9f85b
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2021-08-11 10:35:36 +0600

    Add files via upload

---
+++ Loop/SyntaxForWhileLoop/src/syntaxforwhileloop/SyntaxForWhileLoop.java
@@ -0,0 +1,15 @@
+
+package syntaxforwhileloop;
+
+public class SyntaxForWhileLoop {
+
+    public static void main(String[] args) {
+        int i=1;
+        while(i<6)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }   
+}
+
